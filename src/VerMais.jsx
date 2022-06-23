3b1dd7d5ab17c4573387aab11a975bf6ee19ffba
EzeqCM <rpcnet02@gmail.com>
2022-06-23 02:34:57 -0300

    Mostrar Mais Adicionado

--- src/VerMais.jsx
+++ src/VerMais.jsx
@@ -2,7 +2,7 @@ import './VerMais.css'
 function VerMais(){
     
     return(
-        <p className='VerMais' href="#" onclick="myFunction();">Mostrar Mais</p>
+        <p><a className='VerMais' href="#" onclick="myFunction();">Mostrar Mais</a></p>
     );
 }
 export default VerMais;
