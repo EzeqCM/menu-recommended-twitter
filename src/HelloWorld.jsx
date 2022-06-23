568cac62a0cfdcc1ae396e72f954052a5f8b23e4
EzeqCM <rpcnet02@gmail.com>
2022-06-23 03:07:01 -0300

    Fundo adicionado

--- src/HelloWorld.jsx
+++ src/HelloWorld.jsx
@@ -2,7 +2,7 @@ import './HelloWorld.css'
 function HelloWorld(){
     
     return(
-        <h3 className="titulo">Talvez você curta</h3>
+        <h2 className="titulo">Talvez você curta</h2>
     );
 }
 export default HelloWorld;
