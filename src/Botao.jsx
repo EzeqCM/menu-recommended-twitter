a94b61a56702425fc6f2de4f93435b87595fcae6
EzeqCM <rpcnet02@gmail.com>
2022-06-23 01:38:37 -0300

    Botão Seguir Adicionado

---
+++ src/Botao.jsx
@@ -0,0 +1,13 @@
+import './Botao.css'
+function Botao(){
+    
+    return(
+            
+             <div className='EspacoBotao'> <button className='BotaoEdit'>Seguir</button></div> 
+          
+            
+        
+        
+    );
+}
+export default Botao;
